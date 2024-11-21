c5c551a6359deaef9b143d2fe811546ca85e6e77
shlomo <shlomogratzer1@gmail.com>
2024-11-21 19:35:36 +0200

    disale midelwere

--- Server/src/Routers/QuisRouter.ts
+++ Server/src/Routers/QuisRouter.ts
@@ -1,7 +1,6 @@
 import express, { IRouter } from "express";
 import postController from "../Controller/QuisController"
 import authController from "../Controller/authController"
-import { authMiddleware } from "../middleware/authMiddleware";
 
 const router : IRouter = express.Router()
 router.use("/post" ,authMiddleware, postController)
