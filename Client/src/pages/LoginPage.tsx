f3a58d6c5a9c258522562f7ec97d9b652dad12ff
shlomo <shlomogratzer1@gmail.com>
2024-11-24 16:30:27 +0200

    location popup

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -1,18 +1,9 @@
-import { useContext, useEffect } from 'react'
-import { BooleanProps, isKeyPressContext } from '../Provider/CookieProvider'
-import DialogComp from '../components/dialog-comp/DialogComp'
 import LoginComp from '../components/loginComp/LoginComp'
 
 const LoginPage = () => {
-  const isAuth = useContext<BooleanProps>(isKeyPressContext)
-  useEffect(()=>{
-    console.log(45);
-    console.log(isAuth.isPress);
-  },[isAuth.isPress])
-
+  
   return (
     <>
-    {isAuth.isPress && <DialogComp/>}
     <h1>LoginPage</h1>
     <LoginComp/>
     </>
