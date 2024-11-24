4dba00e9d66da0dd0fb0510bea8cd01f02f52bbf
yosef-chaim <yoske208@gmail.com>
2024-11-24 15:04:25 +0200

    change css and netiv

--- Client/src/pages/Users.tsx
+++ Client/src/pages/Users.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import PageHeader from './PageHeader'
+import AddNewUser from './AddNewUser'
+import { Link } from 'react-router-dom'
 
 export default function Users() {
   return (
     <div>
       <PageHeader title='USERS' />
+      <button><Link to={"/AddNewUser"}>new user</Link></button>
+      
+      
     </div>
   )
 }
--- Client/src/pages/welcome/Welcome.tsx
+++ Client/src/pages/welcome/Welcome.tsx
@@ -5,7 +5,7 @@ import PageHeader from "../PageHeader";
 export default function Welcome() {
   return (
     <div>
-      <PageHeader title="WELCOME TO THE APP" />
+      <PageHeader title="WELCOME TO EINSTALK" />
 
       <img src="Einstalk.webp" alt="Einstalk" />
     </div>
