e8b9d2c0092672febfa337b5696bc445691ce037
shlomo <shlomogratzer1@gmail.com>
2024-11-24 10:15:54 +0200

    CociProvider

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import CociProvider from './Provider/CociProvider'
 import AppRoute from './routers/AppRoute'
 
 function App() {
 
   return (
     <>
+    <CociProvider>
     <AppRoute/>
+    </CociProvider>
     </>
   )
 }
