08c830aad5e22f32af01f4dbf622ada0a0510b2e
kobikru <kobikru@gmail.com>
2024-11-25 11:19:20 +0200

    new welcome

--- Client/src/routers/AppRoute.tsx
+++ Client/src/routers/AppRoute.tsx
@@ -8,20 +8,22 @@ import Profile from "../pages/profile-page/Profile";
 import AddNewUser from "../pages/AddNewUser";
 import NotFound from "../pages/NotFound";
 import LoginPage from "../pages/LoginPage";
-import DialogComp from '../components/dialog-comp/DialogComp'
+import DialogComp from "../components/dialog-comp/DialogComp";
 import { useContext, useEffect } from "react";
 import { BooleanProps, isKeyPressContext } from "../Provider/CookieProvider";
 
 export default function AppRoute() {
-  const isAuth = useContext<BooleanProps>(isKeyPressContext)
-  useEffect(()=>{
+  const isAuth = useContext<BooleanProps>(isKeyPressContext);
+  useEffect(() => {
     console.log(45);
     console.log(isAuth.isPress);
-  },[isAuth.isPress])
+  }, [isAuth.isPress]);
   return (
     <div>
-      {isAuth.isPress && <DialogComp/>}
+      {isAuth.isPress && <DialogComp />}
       <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/welcome" element={<Welcome />}></Route>
         <Route path="/puzzle/:id" element={<Puzzle />}></Route>
