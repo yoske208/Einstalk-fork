46a549ae8e03d8c1bc25da058e78fe5c320f097c
yosef-chaim <yoske208@gmail.com>
2024-11-25 14:33:46 +0200

    change button new puzzele

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import DisplayPuzzles from '../components/DisplayPuzzles'
 import { PuzzelContext } from '../Provider/PuzzelsProvider'
 import PageHeader from './PageHeader'
 import useFatch from '../Hooks/useFetch'
+import { Link } from 'react-router-dom'
 
 
 export default function Home() {
@@ -11,7 +12,9 @@ export default function Home() {
     return (
       <div>
         <h2>What puzzle will you be able to solve today?</h2>
+        <button><Link to={"/puzzle/AddNewPuzzle"} >new puzzle</Link></button>
         <DisplayPuzzles puzzles={puzzels}/>
+
       </div>
     )
   }
